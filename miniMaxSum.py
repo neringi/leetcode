75e4fd5014639280566686489d93d7bd39709d37
Neringa Chomanskyte <chomanskyten@gmail.com>
2024-09-30 16:46:57 +0100

    hackerrank miniMaxSum completed

---
+++ miniMaxSum.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# HackerRank 
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+# Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+# 
+# A single line of five space-separated integers.
+
+
+def miniMaxSum(arr):
+    arr.sort()
+    print('sorted array: ', arr)
+    min = sum(arr[0:4])
+    max = sum(arr[1:5])
+
+    return print(min, max)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
--- plusMinus.py
+++ plusMinus.py
@@ -4,12 +4,14 @@ import random
 import re
 import sys
 
-#
-# Complete the 'plusMinus' function below.
+# HackerRank 
+# 
+# Completed the 'plusMinus' function below.
 #
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 # Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
+# 
 # The first line contains an integer, n, the size of the array.
 # The second line contains  space-separated integers that describe arr[n].
 
