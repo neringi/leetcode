6cda849e25567842d77b59fa05e4eece97247780
Neringa Chomanskyte <chomanskyten@gmail.com>
2025-09-24 08:13:38 +0100

    starting php exercises

---
+++ exercism/php.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Practice</title>
+    </head>
+    <body>
+        <?php echo '<p>Exercism Practice</p>'; ?>
+        <!-- Reverse a string -->
+        <?php $a='stressed';
+        $b = strrev($a);
+        echo "Reversed string {$a} to: {$b}";
+        ?>
+        <!-- resistor color lookup (arrays) -->
+        $resistorcolor = [
+            "black" => 0
+            "brown" => 1
+            "red" => 2
+            "orange" => 3
+            "yellow" => 4
+            "green" => 5
+            "blue" => 6
+            "violet" => 7
+            "grey" => 8
+            "white" => 9
+            ]
+        
+    </body>
+</html>
